15ee52b8420f5b61ac29e14f02b87c56987b3f01
Shriya <iseshriya@gmail.com>
2025-03-27 17:03:14 +0530

    commit1(27/3)

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 import Header from "./components/header";
 import CuisineCards from "./components/CuisineCards";
-import Footer from "./components/footer"; 
-
+import Footer from "./components/footer";
+import axios from "axios";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios.get("http://127.0.0.1:5000/api/hello") // Call Flask API
+      .then(response => setMessage(response.data.message))
+      .catch(error => console.error("Error fetching data:", error));
+  }, []);
+
   return (
     <div>
-      <Header/>
-      <CuisineCards/>
+      <Header />
+      <CuisineCards />
+      <h2>{message ? message : "Loading..."}</h2> {/* Display API message */}
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
